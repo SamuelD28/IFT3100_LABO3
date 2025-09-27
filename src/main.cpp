c5012b47c2fc6e16a0ca9d548c72cb9bea1c8df4
samuel dube <samuel.dube.10@ulaval.ca>
2025-09-27 19:16:23 +0100

    wip: setting up context window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <glad.h>
+#include <GLFW/glfw3.h>
 
-int main()
+void glfwErrorCallback(int errorCode, const char *description)
 {
-	std::cout << "Hello world";
+	std::cout << "An error occured. errorCode =" << errorCode << " description: " << *description << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+	if (!glfwInit())
+	{
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	glfwSetErrorCallback(glfwErrorCallback);
+
+	GLFWwindow *window = glfwCreateWindow(1000, 1000, "Labo 3", NULL, NULL);
+	if (window == NULL)
+	{
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		return -1;
+	}
 }
