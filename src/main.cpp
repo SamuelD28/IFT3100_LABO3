df03e7945fba5c8b63286b89d53805b181b4d446
samuel dube <sdube@aseq.com>
2025-09-27 16:33:31 -0400

    fix: install libraries

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 void glfwErrorCallback(int errorCode, const char *description)
 {
-	std::cout << "An error occured. errorCode =" << errorCode << " description: " << *description << std::endl;
+	std::cout << "\nAn error occured. errorCode =" << errorCode << " description: " << *description << std::endl;
 }
 
 int main(int argc, char *argv[])
@@ -18,18 +18,20 @@ int main(int argc, char *argv[])
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	glfwSetErrorCallback(glfwErrorCallback);
-
 	GLFWwindow *window = glfwCreateWindow(1000, 1000, "Labo 3", NULL, NULL);
 	if (window == NULL)
 	{
 		return -1;
 	}
 
+	glfwSetErrorCallback(glfwErrorCallback);
 	glfwMakeContextCurrent(window);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
+		std::cout << "could not initialised glad" << std::endl;
 		return -1;
 	}
+
+	std::cout << "Ran without issues" << std::endl;
 }
