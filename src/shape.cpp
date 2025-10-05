bdccf61787df75e76613680c3d99ded9b94e3cf1
samuel dube <sdube@aseq.com>
2025-10-05 11:01:09 -0400

    wip: shape api

--- src/shape.cpp
+++ src/shape.cpp
@@ -10,6 +10,39 @@ namespace shape
 		float y = 0;
 	};
 
+	struct triangle
+	{
+	public:
+		point first;
+
+	public:
+		point second;
+
+	public:
+		point third;
+
+		float *getVertices()
+		{
+			float *vertices = new float[8];
+			vertices[0] = first.x;
+			vertices[1] = first.y;
+			vertices[2] = second.x;
+			vertices[3] = second.y;
+			vertices[4] = third.x;
+			vertices[5] = third.y;
+			return vertices;
+		}
+
+		float *getIndices()
+		{
+			float *indices = new float[6];
+			indices[0] = 0;
+			indices[1] = 1;
+			indices[2] = 2;
+			return indices;
+		}
+	};
+
 	struct plane
 	{
 	public:
@@ -26,30 +59,28 @@ namespace shape
 
 		float *getVertices()
 		{
-			float vertices[] = {
-					lowerLeft.x,
-					lowerLeft.y,
-					lowerRight.x,
-					lowerRight.y,
-					upperLeft.x,
-					upperLeft.y,
-					upperRight.x,
-					upperLeft.x,
-			};
+			float *vertices = new float[8];
+			vertices[0] = lowerLeft.x;
+			vertices[1] = lowerLeft.y;
+			vertices[2] = lowerRight.x;
+			vertices[3] = lowerRight.y;
+			vertices[4] = upperLeft.x;
+			vertices[5] = upperLeft.y;
+			vertices[6] = upperRight.x;
+			vertices[7] = upperLeft.x;
 			return vertices;
 		}
 
 		float *getIndices()
 		{
-			float vertices[] = {
-					0,
-					1,
-					2,
-					2,
-					3,
-					1,
-			};
-			return vertices;
+			float *indices = new float[6];
+			indices[0] = 0;
+			indices[1] = 1;
+			indices[2] = 2;
+			indices[3] = 2;
+			indices[4] = 3;
+			indices[5] = 1;
+			return indices;
 		}
 	};
 
